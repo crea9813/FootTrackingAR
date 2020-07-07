11b9b69cda310cbf528cdd5030dd26f44a2b4f45
Yang <crea9813@gmail.com>
2020-07-07 14:00:30 +0900

    Initial Commit

---
+++ FootTracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FootTracking
+//
+//  Created by Yang on 2020/07/07.
+//  Copyright © 2020 Minestrone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
